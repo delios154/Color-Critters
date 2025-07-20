10445f071f3dee7829eefe1f813340a65984e4a1
delios154 <almansoori.154@gmail.com>
2025-07-20 17:28:18 +0400

    Initial Commit

---
+++ Color Critters!Tests/Color_Critters_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Color_Critters_Tests.swift
+//  Color Critters!Tests
+//
+//  Created by Mohammed Almansoori on 20/07/2025.
+//
+
+import Testing
+@testable import Color_Critters_
+
+struct Color_Critters_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Color Critters!UITests/Color_Critters_UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Color_Critters_UITestsLaunchTests.swift
+//  Color Critters!UITests
+//
+//  Created by Mohammed Almansoori on 20/07/2025.
+//
+
+import XCTest
+
+final class Color_Critters_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
